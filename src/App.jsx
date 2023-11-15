307492f4361bb64f3f506443bafb99118770073b
Anelize Nardelli <126248990+N4rdelli@users.noreply.github.com>
2023-11-15 23:01:12 +0000

    Header básico feito. Precisa: mudar as cores dos ícones e adicionar hover.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 import styles from "./App.css";
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
-import Header from './components/Header/Header';
-import Library from './components/Library/Library';
+import Header from "./components/Header/Header";
+import Library from "./components/Library/Library";
 
 const App = () => {
   return (
     <>
-    <div>
-      <Header />
-      <Library />
-    </div>
-    <div>
-      <Outlet />
-    </div>
+      <div className="container">
+        <div classname="left">
+          <Header />
+          <Library />
+        </div>
+        <div className="right">
+          <Outlet />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,42 @@
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
+import iconHome from "../../assets/home-outline.svg";
+import iconSearch from "../../assets/search-outline.svg";
+import logoImg from "../../assets/spotify.svg";
 
 import { Link } from "react-router-dom";
 
 const navigation = [
-    {component: "/", name: "Início"},
-    {component: "/search", name: "Buscar"}
-]
-
+  {
+    component: "/",
+    name: "Início",
+    icon: iconHome,
+    alt: "Ícone de uma casa",
+  },
+  {
+    component: "/search",
+    name: "Buscar",
+    icon: iconSearch,
+    alt: "Ícone de uma lupa",
+  },
+];
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <img src={logoImg} alt="Logo Spotify"/>
+        <h1> Spotify </h1>
+      </div>
+      
+      <div className={styles.nav}>
+        {navigation.map((item) => (
+          <Link key={item.name} to={item.component}>
+            <img src={item.icon} alt={item.alt} /> {item.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
